156b17e57f03220b38d2dd2dad9ec8a81e8bd041
GameTagger507 <94531959+GameTagger507@users.noreply.github.com>
2023-11-22 15:59:07 +0100

    build gradle ins git inkludiert.

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+dependencies {
+
+    //GSON ist eine Bibliothek zum Schreiben und Lesen
+    // https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
